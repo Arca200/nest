24738a62b377fb335311c82b15061c3a085e3130
Arca200 <wu.xinhai@outlook.com>
2024-04-11 16:36:08 +0800

    04/11

---
+++ entities/index.ts
@@ -0,0 +1,12 @@
+export {
+    User,
+} from './entities/User'
+export {
+    Profile,
+} from './entities/Profile'
+export {
+    Log,
+} from './entities/Log'
+export {
+    Role,
+} from './entities/Role'
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
-import { UserService } from './user.service';
-import { ConfigService } from '@nestjs/config';
+import { Controller, Get } from '@nestjs/common'
+import { UserService } from './user.service'
+import { ConfigService } from '@nestjs/config'
 
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService, private configService: ConfigService) {
-  }
+  constructor(
+    private userService: UserService,
+    private configService: ConfigService,
+  ) {}
 
   @Get('getUser')
   getUser() {
-    console.log(this.configService.get('DB'));
-    return this.userService.getUser();
+    return this.userService.getUser()
   }
 }
